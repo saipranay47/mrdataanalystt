1e26cf4bc957eb84de3d89722bf6794e664ce6f5
saipranay47 <saipranayadepu07@gmail.com>
2024-12-30 16:47:09 +0530

    fix product links

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -24,33 +24,33 @@ export default function ProductsPage() {
 
       <div className="mt-16 grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
         {products.map((product) => (
-          <div
-            key={product.id}
-            className="group relative overflow-hidden rounded-lg border bg-background p-2"
-          >
-            <div className="relative aspect-[4/3] overflow-hidden rounded-md">
-              <Image
-                src={product.image}
-                alt={product.title}
-                fill
-                className="object-cover transition-transform group-hover:scale-105"
-              />
-              <Badge className="absolute right-2 top-2 ">{product.price}</Badge>
-            </div>
-            <div className="p-4">
-              <h2 className="text-xl font-semibold">{product.title}</h2>
-              <p className="mt-2 text-sm text-muted-foreground">
-                {product.description}
-              </p>
-              <div className="mt-4 flex gap-2">
-                <Button size="sm" asChild className="w-full">
-                  <Link href={product.purchaseLink} target="_blank">
-                    Grab Now
-                  </Link>
+          <Link href={`/products/${product.id}`} key={product.id}>
+            <div
+              key={product.id}
+              className="group relative overflow-hidden rounded-lg border bg-background p-2"
+            >
+              <div className="relative aspect-[4/3] overflow-hidden rounded-md">
+                <Image
+                  src={product.image}
+                  alt={product.title}
+                  fill
+                  className="object-cover transition-transform group-hover:scale-105"
+                />
+                <Badge className="absolute right-2 top-2 ">
+                  {product.price}
+                </Badge>
+              </div>
+              <div className="p-4">
+                <h2 className="text-xl font-semibold">{product.title}</h2>
+                <p className="mt-2 text-sm text-muted-foreground">
+                  {product.description}
+                </p>
+                <Button size="sm" className="w-full">
+                  Grab Now
                 </Button>
               </div>
             </div>
-          </div>
+          </Link>
         ))}
       </div>
     </div>
